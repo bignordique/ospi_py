8a5c8d3cd2f2553720b98fef798ea256e519fcf2
leith <leith.johnson@gmail.com>
2025-02-12 14:07:33 -0700

    More install and fooling around with lighttpd.conf

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ sudo ln -s /home/leith/ospi_py/src/cgi-bin/ospi_fcgi.py /var/www/cgi-bin/ospi_py
 sudo mkdir -p /srv/ospi_py/water_logs
 sudo chown -R wwww-data:www-data /srv/ospi_py
 
-sudo mkdir -p /var/www/html/ospi_py
+sudo ln -s /home/leith/ospi_py/html ospi_py
+
+sudo ls -sf /home/leith/ospi_py/config/lighttpd.conf /etc/lighttpd
+
+
 
